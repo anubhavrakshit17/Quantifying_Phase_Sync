db33edb60c10fac84d616e5c81d2869d69142e32
Anubhav Rakshit <38507672+anubhavrakshit17@users.noreply.github.com>
2022-10-26 21:29:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ Created on Sun Oct 23 15:20:28 2022
 @author: Anubhav
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 16 20:00:27 2022
+
+@author: Anubhav
+"""
 
 
 import matplotlib.pyplot as plt
@@ -132,7 +138,7 @@ ii_raw_ipsi=raw_ipsi[ss[0]:ss[1]]
 ii_raw_contra = raw_contra[ss[0]:ss[1]]
 iit= t[ss[0]:ss[1]]
 
-# Visualize the interictal data
+#%% Time domain - Raw interictal
 
 f,ax = plt.subplots(2,1,facecolor='w',figsize=(16,3),sharex= True) # sharex zooms together both subplots 
 f.subplots_adjust(left=0.04,right=0.99,top=0.98,bottom=0.2)
@@ -140,6 +146,30 @@ ax[0].plot(iit,ii_raw_ipsi,linewidth=0.5,color='black')
 ax[0].set_ylim(-5,5)
 ax[1].plot(iit,ii_raw_contra,linewidth=0.5,color='black')
 ax[1].set_ylim(-5,5)
+#%% frequency domain - Raw interictal
+
+x= ii_raw_ipsi
+x = x - x.mean()          # Subtract the mean from the data.
+N = len(x)                 # Define the number of points in the trial.
+xh = hanning(N) * x       # Multiply data by Hanning window,
+xf = rfft(xh)[:-1]        # ... compute Fourier transform,
+
+dt = t[1] - t[0]           # Define the sampling interval.
+T = len(iit) / sampling_freq # Define the duration of the trial.
+df = 1 / T;               # Determine frequency resolution.
+fNQ = 1 / dt / 2;         # Determine Nyquist frequency.
+Sxx = 2 * dt ** 2 / T * (xf * xf.conj());  #... and compute spectrum.
+
+faxis = arange(0, fNQ, df) # Construct frequency axis
+faxis= np.delete(faxis, 0) #dropping the first element, because of length mismatch
+
+plt.figure()
+semilogx(faxis, 10 * log10(Sxx.real))   # Plot decibels vs frequency,
+xlim([df, 100])           # ... in limited frequency range,
+xlabel('Frequency [Hz]')  # ... with axes labeled.
+ylabel('Power [dB]')
+title('Single-trial spectrum')
+grid('on')
 
 #%% For MATLAB Conversion [Optional]
 # raw_ipsi_m = transpose(np.reshape(raw_ipsi, (len(raw),1)))
@@ -156,30 +186,56 @@ ax[1].set_ylim(-5,5)
 #%%
 
 del raw_all_channels
-#%%
-x= ii_raw_ipsi
-x = x - x.mean()          # Subtract the mean from the data.
-N = len(x)                 # Define the number of points in the trial.
-xh = hanning(N) * x       # Multiply data by Hanning window,
-xf = rfft(xh)[:-1]        # ... compute Fourier transform,
 
-dt = t[1] - t[0]           # Define the sampling interval.
-T = len(iit) / sampling_freq # Define the duration of the trial.
-df = 1 / T;               # Determine frequency resolution.
-fNQ = 1 / dt / 2;         # Determine Nyquist frequency.
-Sxx = 2 * dt ** 2 / T * (xf * xf.conj());  #... and compute spectrum.
+#%% Filtering (Not used in python but in MATLAB, subject of investigation)
+# Next step is Filtering.  Filtering is convolution of a window and the wave. 
+# Visualize the window of the filter
 
-faxis = arange(0, fNQ, df) # Construct frequency axis
-faxis= np.delete(faxis, 0) #dropping the first element, because of length mismatch
-#%% Visualizing in frequency domain
+filter = np.float32(signal.firwin(400, [0.0004, 0.0010], 
+                                  pass_zero=False))
+
+# First we convolve then we convert to float32 because 
+#float64 hangs the prog.
+
+ii_filt_ipsi = np.float32(signal.convolve(ii_raw_ipsi, filter, 
+                                       mode='same'))
+ii_filt_contra = np.float32(signal.convolve(ii_raw_contra, 
+                                         filter, mode='same'))
+#%% Time domain - Filtered interictal
+plt.figure()
+plt.plot(iit,ii_filt_ipsi,linewidth=0.5)
+
+#%% Frequency Domain - Filtered interictal
+
+fil_x= ii_filt_ipsi
+fil_x = fil_x - fil_x.mean()          # Subtract the mean from the data.
+fil_N = len(fil_x)                 # Define the number of points in the trial.
+fil_xh = hanning(fil_N) * fil_x       # Multiply data by Hanning window,
+fil_xf = rfft(fil_xh)[:-1]        # ... compute Fourier transform,
+
+fil_dt = t[1] - t[0]           # Define the sampling interval.
+fil_T = len(iit) / sampling_freq # Define the duration of the trial.
+fil_df = 1 / fil_T;               # Determine frequency resolution.
+fil_fNQ = 1 / fil_dt / 2;         # Determine Nyquist frequency.
+fil_Sxx = 2 * fil_dt ** 2 / fil_T * (fil_xf * fil_xf.conj());  #... and compute spectrum.
+
+fil_faxis = arange(0, fil_fNQ, fil_df) # Construct frequency axis
+fil_faxis= np.delete(fil_faxis, 0) #dropping the first element, because of length mismatch
+# Visualizing in frequency domain
 
 plt.figure()
-semilogx(faxis, 10 * log10(Sxx.real))   # Plot decibels vs frequency,
-xlim([df, 100])           # ... in limited frequency range,
+semilogx(fil_faxis, 10 * log10(fil_Sxx.real))   # Plot decibels vs frequency,
+xlim([fil_df, 100])           # ... in limited frequency range,
 xlabel('Frequency [Hz]')  # ... with axes labeled.
 ylabel('Power [dB]')
 title('Single-trial spectrum')
 grid('on')
+
+#%% Comparing both filtered and raw ipsilateral segments
+ff,axf = plt.subplots(2,1)
+axf[0].semilogx(faxis, 10 * log10(Sxx.real))
+axf[1].semilogx(fil_faxis, 10 * log10(fil_Sxx.real))
+
 #%%
 
 
